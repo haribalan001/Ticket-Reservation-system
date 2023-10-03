85f6c04b2942db86cf098fbe0df70a86e4c13bda
haribalan001 <116061499+haribalan001@users.noreply.github.com>
2023-10-03 16:46:53 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location.assign("index.html");
+}
+function tickets(){
+    window.location.assign("tickets.html");
+}
+function user(){
+    window.location.assign("user.html");
+}
+function support(){
+    window.location.assign("support.html");
+}
+function settings(){
+    window.location.assign("setting.html")
+}
